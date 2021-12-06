9dc94ecf80cee165e6d1cb3cb4dc14e5a740c8d0
Alexander Rose <alexander.rose@codecentric.de>
2021-12-06 16:11:25 +0100

    Day 6 Part I.

---
+++ day-6/part1.js
@@ -0,0 +1,27 @@
+const currentFishes = require("./input.json");
+
+function nextDay(fishes) {
+  return fishes.reduce((fishesOfNextDay, fish) => {
+    if (fish === 0) {
+      fishesOfNextDay.push(6);
+      fishesOfNextDay.push(8);
+    } else {
+      fishesOfNextDay.push(fish - 1);
+    }
+    return fishesOfNextDay;
+  }, []);
+}
+
+function calculateFishPopulation(fishes, numberOfDays) {
+  return range(1, numberOfDays).reduce((population) => {
+    return nextDay(population);
+  }, fishes);
+}
+
+function range(start, end) {
+  return Array(end - start + 1)
+    .fill()
+    .map((_, index) => start + index);
+}
+
+console.log(calculateFishPopulation(currentFishes, 80).length);
