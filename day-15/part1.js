5dba59fc8ddb55e4ac9a135a107aa5f27d8f34b5
Alexander Rose <alexander.rose@codecentric.de>
2021-12-15 12:57:14 +0100

    Cleanup.

--- day-15/part1.js
+++ day-15/part1.js
@@ -1,44 +1,62 @@
 const input = require("./input.json");
 
 function minimumRiskOf(map) {
-  const bottom = map.length - 1;
-  const right = map[0].length - 1;
-  const end = `${bottom}.${right}`;
+  const [maxY, maxX] = maxOf(map);
 
   const riskFactors = {};
   const queue = [[0, 0, 0]];
-  const visited = new Set();
+  const visitedCells = new Set();
 
-  while (queue.length) {
-    const [riskFactor, y, x] = queue.shift();
-    const key = `${y}.${x}`;
-    const currentRisk = riskFactors[key] || Infinity;
+  while (queue.length > 0) {
+    const [riskFactor, x, y] = queue.shift();
+    const key = keyOf(x, y);
 
-    if (!visited.has(key)) {
-      visited.add(key);
+    if (!visitedCells.has(key)) {
+      visitedCells.add(key);
       riskFactors[key] = riskFactor;
 
-      for (const [dx, dy] of [
-        [0, 1],
-        [0, -1],
-        [1, 0],
-        [-1, 0],
-      ]) {
-        const newX = x + dx;
-        const newY = y + dy;
+      adjacentCells(x, y)
+        .filter(inMap(maxX, maxY))
+        .forEach(addToQueue(queue, riskFactor, map));
 
-        if (newY >= 0 && newY <= bottom && newX <= right && newX >= 0)
-          queue.push([map[newY][newX] + riskFactor, newY, newX]);
-      }
-
-      queue.sort((a, b) => a[0] - b[0]);
+      queue.sort(byRisk);
     }
   }
 
-  return riskFactors[end];
+  return riskFactors[keyOf(maxX, maxY)];
 }
 
-//console.log(minimumRiskOf(input));
+function maxOf(map) {
+  return [map.length - 1, map[0].length - 1];
+}
+
+function keyOf(x, y) {
+  return `${y},${x}`;
+}
+
+function adjacentCells(x, y) {
+  return [
+    [x, y + 1],
+    [x, y - 1],
+    [x + 1, y],
+    [x + 1, y],
+  ];
+}
+
+function addToQueue(queue, riskFactor, map) {
+  return ([newX, newY]) =>
+    queue.push([map[newY][newX] + riskFactor, newX, newY]);
+}
+
+function byRisk(a, b) {
+  return a[0] - b[0];
+}
+
+function inMap(maxX, maxY) {
+  return ([x, y]) => y >= 0 && y <= maxY && x <= maxX && x >= 0;
+}
+
+console.log(minimumRiskOf(input));
 
 module.exports = {
   minimumRiskOf,
