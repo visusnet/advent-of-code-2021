1d1d8f5f1ad1e3428b561f7b4da72d8a90548a18
Alexander Rose <alexander.rose@codecentric.de>
2021-12-21 00:04:45 +0100

    Cleanup.

--- day-20/part1.js
+++ day-20/part1.js
@@ -12,42 +12,39 @@ function applyAlgorithm(image, algorithm, numberOfEnhancements) {
 function enhanceImage(image, algorithm) {
   const [width, height] = [image[0].length - PADDING, image.length - PADDING];
   return range(1, height).map((rowIndex) =>
-    range(1, width).map((columnIndex) => {
-      return algorithm[
-        toAlgorithmIndex(squareOf(image, rowIndex, columnIndex))
-      ];
-    })
-  );
-}
-
-function pad(image, numberOfEnhancements) {
-  const padding = PADDING * numberOfEnhancements;
-  const blankImage = blankImageWith(
-    image[0].length + 2 * padding,
-    image.length + 2 * padding
-  );
-
-  return blankImage.map((row, rowIndex) =>
-    row.map((pixel, columnIndex) =>
-      image[rowIndex - padding] &&
-      image[rowIndex - padding][columnIndex - padding]
-        ? image[rowIndex - padding][columnIndex - padding]
-        : pixel
+    range(1, width).map(
+      (columnIndex) =>
+        algorithm[toAlgorithmIndex(squareOf(image, rowIndex, columnIndex))]
     )
   );
 }
 
+function pad(image, numberOfEnhancements) {
+  const paddingPerSide = numberOfEnhancements * PADDING;
+  const padding = paddingPerSide * 2;
+  return blankImageWith(image[0].length + padding, image.length + padding).map(
+    (row, rowIndex) =>
+      row.map(
+        (pixel, columnIndex) =>
+          image[rowIndex - paddingPerSide]?.[columnIndex - paddingPerSide] ??
+          pixel
+      )
+  );
+}
+
 function unpad(image, numberOfEnhancements) {
-  const padding = numberOfEnhancements * PADDING;
-  const paddingPerSide = padding / 2;
+  const paddingPerSide = numberOfEnhancements * PADDING;
+  const paddingOffset = paddingPerSide / 2;
   const [width, height] = [image[0].length - 1, image.length - 1];
-  const rowDimensions = [paddingPerSide, height - paddingPerSide];
-  const columnDimensions = [paddingPerSide, width - paddingPerSide];
-  const inDimensions = (dimensions) => (_, index) =>
-    dimensions[0] <= index && index <= dimensions[1];
+  const rowDimensions = [paddingOffset, height - paddingOffset];
+  const columnDimensions = [paddingOffset, width - paddingOffset];
   return image
-    .map((row) => row.filter(inDimensions(columnDimensions)))
-    .filter(inDimensions(rowDimensions));
+    .filter(inDimensions(rowDimensions))
+    .map((row) => row.filter(inDimensions(columnDimensions)));
+}
+
+function inDimensions(dimensions) {
+  return (_, index) => dimensions[0] <= index && index <= dimensions[1];
 }
 
 function blankImageWith(width, height) {
@@ -57,7 +54,7 @@ function blankImageWith(width, height) {
 }
 
 function squareOf(image, rowIndex, columnIndex) {
-  const squareCoordinates = [
+  return [
     [rowIndex - 1, columnIndex - 1],
     [rowIndex - 1, columnIndex],
     [rowIndex - 1, columnIndex + 1],
@@ -67,11 +64,7 @@ function squareOf(image, rowIndex, columnIndex) {
     [rowIndex + 1, columnIndex - 1],
     [rowIndex + 1, columnIndex],
     [rowIndex + 1, columnIndex + 1],
-  ];
-
-  return squareCoordinates.map(([y, x]) =>
-    image[y] && image[y][x] ? image[y][x] : "."
-  );
+  ].map(([y, x]) => image[y]?.[x] ?? ".");
 }
 
 function toAlgorithmIndex(square) {
